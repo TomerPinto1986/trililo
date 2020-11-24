1757bc139fcb961f6e9cb94ccf8be64f0842203a
Tomer Pinto <t.pintoosi@gmail.com>
2020-11-24 21:30:56 +0200

    fix service

---
+++ src/services/board.service.js
@@ -0,0 +1,33 @@
+import httpService from './http.service';
+
+
+const BASE_URL = (process.env.NODE_ENV !== 'development') ? '/api/board' : '//localhost:3030/api/board';
+
+const KEY = 'board';
+
+export const toyService = {
+    query,
+    getById,
+    remove,
+    save,
+};
+
+function query() {
+    return httpService.get('board');
+}
+
+function getById(boardId) {
+    return httpService.get(`board/${boardId}`);
+}
+
+function remove(boardId) {
+    return httpService.delete(`board/${boardId}`);
+}
+
+function save(board) {
+    if (board._id) {
+        return httpService.put(`board/${board._id}`, board);
+    } else {
+        return httpService.post(`board`, board);
+    }
+}
