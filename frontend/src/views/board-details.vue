ec6f073d24b69705065e24e186b4704dfa642d4d
Tomer Pinto <t.pintoosi@gmail.com>
2020-11-25 00:35:53 +0200

    board details

---
+++ frontend/src/views/board-details.vue
@@ -0,0 +1,36 @@
+<template>
+    <section class="board-details">
+        <h1>details</h1>
+        <!-- <board-header :board="board"> -->
+        <template v-if="board">
+            <div v-for="cardList in board.lists" :key="cardList.id">
+                <card-list :cardList="cardList" />
+            </div>
+        </template>
+    </section>
+</template>
+
+<script>
+import { boardService } from '../services/board.service.js';
+import cardList from '../cmps/card/card-list.cmp';
+
+export default {
+    data() {
+        return {
+            board: null
+        }
+    },
+    components: {
+        cardList
+    },
+    async created() {
+        const boardId = this.$route.params.boardId;
+        this.board = await boardService.getById(boardId);
+    }
+
+
+}
+</script>
+
+<style>
+</style>
