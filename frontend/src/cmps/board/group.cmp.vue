a5815d5d86804de1e9218f8d60fd0a2d0a34e491
Tomer Pinto <t.pintoosi@gmail.com>
2020-11-26 00:42:58 +0200

    day2

--- frontend/src/cmps/board/group.cmp.vue
+++ frontend/src/cmps/board/group.cmp.vue
@@ -24,16 +24,6 @@
 				<button>Save</button>
 			</form>
 		</div>
-		<card-preview
-			v-for="card in group.cards"
-			:key="card.id"
-			:card="card"
-			@click.native="openDetails(card.id)"
-		/>
-		<form v-if="isAdding" @submit.prevent="saveCard">
-			<input type="text" v-model="newCardTxt" />
-			<button>Save</button>
-		</form>
 		<button v-if="!isAdding" @click="addCard">+ Add another card</button>
 	
 	</section>
