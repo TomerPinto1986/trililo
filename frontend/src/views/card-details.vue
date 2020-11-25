4fefb9259cedb422bcd3aa460549e656c3b86341
Eden Aran <edenaran@gmail.com>
2020-11-25 12:59:49 +0200

    working card-details save

--- frontend/src/views/card-details.vue
+++ frontend/src/views/card-details.vue
@@ -1,7 +1,17 @@
 <template>
-	<section v-if="card" class="card-details">
-		<button @click="emitClose">x</button>
-		<h2>{{ card.title }}</h2>
+	<section v-if="card" class="card-details flex f-col">
+		<input class="title" type="text" v-model="card.title" />
+		<h3>Description</h3>
+		<input
+			class="desc"
+			type="text"
+			v-model="card.description"
+			placeholder="Add a more detailed description..."
+		/>
+		<div class="btns flex">
+			<button class="save-btn" @click="saveCard">Save</button>
+			<button class="cancel-btn" @click="emitClose">Cancel</button>
+		</div>
 	</section>
 </template>
 
@@ -9,25 +19,25 @@
 export default {
 	data() {
 		return {
-            card: null
-        }
+			card: null
+		}
 	},
 	computed: {},
 	methods: {
-		emitClose(){
-			this.$emit('close')
+		emitClose() {
+			this.$emit('close');
 			this.card = null;
+		},
+		saveCard() {
+			this.$store.dispatch({ type: 'saveCard', card: this.card })
+			this.emitClose();
 		}
 	},
-	
 	created() {
-		const cardId = this.$route.params.cardId
-		this.$store.commit({type:'updateCurrCard', cardId})
-		this.card = this.$store.getters.currCard;	
+		const cardId = this.$route.params.cardId;
+		this.$store.commit({ type: 'setCurrCard', cardId });
+		this.card = JSON.parse(JSON.stringify(this.$store.getters.currCard));
 	}
 
 }
 </script>
-
-<style>
-</style>
