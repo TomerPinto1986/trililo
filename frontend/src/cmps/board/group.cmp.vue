205c3db284c8ee4b4492777a79c3aef9845dd301
Eden Aran <edenaran@gmail.com>
2020-11-26 08:57:12 +0200

    new day

--- frontend/src/cmps/board/group.cmp.vue
+++ frontend/src/cmps/board/group.cmp.vue
@@ -12,20 +12,6 @@
 				<button @click="emitDelete(group.id)">Delete Group</button>
 			</div>
 		</div>
-<<<<<<< HEAD
-		<div class="group-info flex f-center wrap">
-			<card-preview
-				v-for="card in group.cards"
-				:key="card.id"
-				:card="card"
-				@click.native="openDetails(card.id)"
-			/>
-			<form v-if="isAdding" @submit.prevent="saveCard">
-				<input type="text" v-model="newCardTxt" />
-				<button>Save</button>
-			</form>
-		</div>
-=======
 		<draggable v-model="myList" class="group-info flex f-center wrap">
 			<transition-group>
 				<card-preview
@@ -41,7 +27,6 @@
 				</form>
 			</transition-group>
 		</draggable>
->>>>>>> 1df1a6ba95eb8a1056a9b18c724167affef2d74a
 		<button v-if="!isAdding" @click="addCard">+ Add another card</button>
 	</section>
 </template>
@@ -83,22 +68,9 @@ export default {
 		},
 		emitDelete(groupId) {
 			this.$emit('delete', groupId)
-		},
-		haa(ev) {
-			console.log(ev)
 		}
 	},
 	computed: {
-		myList: {
-			get() {
-				console.log('value')
-				return this.$store.state.myList
-			},
-			set(value) {
-				console.log(value)
-				// this.$store.commit('updateList', value)
-			}
-		}
 	},
 	components: {
 		cardPreview,
