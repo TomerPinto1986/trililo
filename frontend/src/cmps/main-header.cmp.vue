5eede44a951a248db5a98fdbda16be049d0c555a
Shifra Sherrow <yy306090@gmail.com>
2020-11-27 08:36:23 +0200

    mm

--- frontend/src/cmps/main-header.cmp.vue
+++ frontend/src/cmps/main-header.cmp.vue
@@ -1,5 +1,4 @@
 <template>
-<<<<<<< HEAD
     <section class="main-header">
         <template v-if="isGuest">
             <router-link to="/login">Login</router-link> |
@@ -7,17 +6,10 @@
         </template>
         <button v-else @click="emitLogout">Logout</button>
     </section>
-=======
-	<section class="main-header" :class="headerClass">
-		<router-link to="/login">Login</router-link> |
-		<router-link to="/signup">Signup</router-link>
-	</section>
->>>>>>> 024c95ced93e27479cbfffbf353acd96643e1fc2
 </template>
 
 <script>
 export default {
-<<<<<<< HEAD
     props: {
         loggedinUser: String
     },
@@ -31,12 +23,5 @@ export default {
             this.$emit('logout');
         }
     }
-=======
-	computed: {
-		headerClass() {
-			return {};
-		}
-	}
->>>>>>> 024c95ced93e27479cbfffbf353acd96643e1fc2
 };
 </script>
