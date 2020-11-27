242116212907fa53897e0b6bbf0d05f7e77ea5e8
Tomer Pinto <t.pintoosi@gmail.com>
2020-11-27 09:48:40 +0200

    lol

--- frontend/src/views/home-page.vue
+++ frontend/src/views/home-page.vue
@@ -1,26 +1,10 @@
 <template>
-<<<<<<< HEAD
-    <section class="home-page">
-
-    </section>
-=======
 <section class="home-page">
     <router-link to="/board">Go to boards :D</router-link>
 </section>
->>>>>>> 024c95ced93e27479cbfffbf353acd96643e1fc2
 </template>
 
 <script>
 
-<<<<<<< HEAD
-export default {
-
-};
-</script>
-=======
 </script>
 
-<style>
-
-</style>
->>>>>>> 024c95ced93e27479cbfffbf353acd96643e1fc2
