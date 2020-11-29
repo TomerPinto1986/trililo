709d34c0f608280125a59f67dccd444d43f79a39
Tomer Pinto <t.pintoosi@gmail.com>
2020-11-30 00:54:45 +0200

    activity

--- frontend/src/cmps/card/card-activity.cmp.vue
+++ frontend/src/cmps/card/card-activity.cmp.vue
@@ -2,12 +2,22 @@
     <section v-if="user" class="card-activity">
         <h2>activity:</h2>
         <button>Show Details</button>
-        <avatar username="user.username"></avatar>
-        <input type="text" placeholder="Write a comment..." @keydown="typing" v-model="msg.txt">
-        <button @click="sendMsg">Send</button>
-        <span>{{chat}}</span>
-        <template v-if="activities && activities.length">
-            <activity-preview v-for="activity in activities" :key="activity.id" :activity="activity" />
+        <div class="flex">
+            <avatar :size="35" :username="user.username"></avatar>
+            <input
+                type="text"
+                placeholder="Write a comment..."
+                @keydown="typing"
+                v-model="msg.txt"
+            />
+            <button @click="sendMsg">Send</button>
+        </div>
+        <template v-if="activitiesToShow && activitiesToShow.length">
+            <activity-preview
+                v-for="activity in activitiesToShow"
+                :key="activity.id"
+                :activity="activity"
+            />
         </template>
     </section>
 </template>
@@ -18,60 +28,77 @@ import avatar from 'vue-avatar';
 import { socketService } from '../../services/socket.service';
 
 export default {
-	props: {
-		activities: Array,
-		user: Object,
-		card: Object
-	},
-	data() {
-		return {
-			msg: {
-				from: null,
-				txt: ''
-			},
-			chat: [],
-			userTyping: null,
-			timeOutTyping: null
-		}
-	},
-	computed: {
-	},
-	methods: {
-		sendMsg() {
-			socketService.emit('chat-newMsg', this.msg)
-			this.msg.txt = '';
-		},
-		addMsg(chat) {
-			this.userTyping = null
-			this.chat = chat
-		},
-		typing() {
-			socketService.emit('typing', this.msg.from)
-		},
-		setUserTyping(username) {
-			this.userTyping = username
-			if (this.timeOutTyping) clearTimeout(this.timeOutTyping)
-			this.timeOutTyping = setTimeout(() => {
-				this.userTyping = null
-			}, 1500)
-		}
-	},
-	created() {
-		
-		this.from = this.user.username;
-		socketService.setup();
-		socketService.emit('card-topic', this.card.id);
-		socketService.on('chat-history', (chat => this.chat = chat))
-		socketService.on('chat-addMsg', this.addMsg);
-		socketService.on('user-typing', this.setUserTyping);
-	},
-	destroyed() {
-		socketService.off('chat-addMsg', this.addMsg)
-		socketService.terminate();
-	},
-	components: {
-		activityPreview,
-		avatar
-	}
+    props: {
+        // activities: Array,
+        user: Object,
+        card: Object
+    },
+    data() {
+        return {
+            msg: {
+                from: this.user.username,
+                txt: ''
+            },
+            userTyping: null,
+            timeOutTyping: null
+        }
+    },
+    computed: {
+        activitiesToShow() {
+			console.log(this.card);
+			console.log(this.card.activities);
+            return (this.card.activities);
+        }
+    },
+    methods: {
+        sendMsg() {
+            socketService.emit('activity-newMsg', this.msg)
+            this.msg.txt = '';
+        },
+        addMsg(msg) {
+            this.userTyping = null;
+            // this.chat = chat
+            const byMember = {
+                _id: this.user._id,
+                username: this.user.username,
+                imgUrl: this.user.imgUrl
+            };
+            const activity = {
+                txt: msg.txt,
+                byMember,
+			};
+			if (!this.card.activities) this.card.activities = [];
+			this.card.activities.push(activity);
+			console.log(this.card,'activ???');
+			this.$emit('addActivity',activity.txt,this.card);
+        },
+        typing() {
+            socketService.emit('typing', this.msg.from)
+        },
+        setUserTyping(username) {
+            this.userTyping = username
+            if (this.timeOutTyping) clearTimeout(this.timeOutTyping)
+            this.timeOutTyping = setTimeout(() => {
+                this.userTyping = null
+            }, 1500)
+        }
+    },
+    created() {
+
+        this.from = this.user.username;
+        socketService.setup();
+        socketService.emit('card-topic', this.card.id);
+        // socketService.on('chat-history', (chat => this.chat = chat))
+        socketService.on('activity-addMsg', this.addMsg);
+        socketService.on('user-typing', this.setUserTyping);
+    },
+    destroyed() {
+        socketService.off('activity-addMsg', this.addMsg)
+        socketService.terminate();
+    },
+    components: {
+        activityPreview,
+        avatar
+    }
 };
 </script>
