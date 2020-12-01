9fa11623976fb68adff28d907bd7c5b3d166b927
Tomer Pinto <t.pintoosi@gmail.com>
2020-12-01 12:36:39 +0200

    homePage start

--- frontend/src/views/home-page.vue
+++ frontend/src/views/home-page.vue
@@ -1,7 +1,12 @@
 <template>
-<section class="home-page">
-    <router-link class="boards-btn" to="/board">Go to boards :D</router-link>
-</section>
+    <section class="home-page">
+        <router-link class="boards-btn" to="/board"
+            >Go to boards :D</router-link
+        >
+        <div>
+            <img src="@/assets/animations/home-page1.gif" alt="" />
+        </div>
+    </section>
 </template>
 
 <script>
