ce611a6a3c031b22afc898bd3fe3bc79bdb46746
Eden Aran <edenaran@gmail.com>
2020-12-03 00:16:00 +0200

    group Menu

--- frontend/src/cmps/board/group.cmp.vue
+++ frontend/src/cmps/board/group.cmp.vue
@@ -6,16 +6,68 @@
 				class="title"
 				v-model="group.title"
 				@change="emitChange"
-				slot="header"
+				@click.stop
 			/>
-			<button @click="toggleMenu">
+			<button @click.stop="toggleMenu">
 				<i class="fas fa-ellipsis-h menu-btn"></i>
 			</button>
 			<group-menu
 				v-if="isMenu"
 				@closeMenu="toggleMenu"
+				@addCard="addCard"
 				@deleteGroup="emitDelete(group.id)"
-			></group-menu>
+				@cloneGroup="cloneGroup"
+				@openMove="openMove"
+				@openSort="openSort"
+				@backToMenu="backToMenu"
+			>
+				<template v-if="isMove || isSort">
+					<i
+						@click="backToMenu"
+						slot="back-btn"
+						class="fas fa-chevron-left back-btn"
+					></i>
+					<template v-if="isMove">
+						<span class="header" slot="header"> Move List </span>
+						<div class="content" slot="body">
+							Move To:
+							<el-select
+								class="move-select"
+								@change="changeGroupPos"
+								v-model="pos"
+							>
+								<el-option
+									v-for="idx in board.groups.length"
+									:key="idx"
+									:label="idx"
+									:value="idx"
+								>
+								</el-option>
+							</el-select>
+						</div>
+					</template>
+					<template v-if="isSort">
+						<span class="header" slot="header"> Sort List </span>
+						<div class="content" slot="body">
+							<div @click="sortBy('createdAt', 1)">
+								Date Created (Newest First)
+							</div>
+							<div @click="sortBy('createdAt', -1)">
+								Date Created (Oldest First)
+							</div>
+							<div @click="sortBy('title', 1)">
+								Card Name (Alphabetically)
+							</div>
+							<!-- <div
+								@click="sortBy('dueDate', -1)"
+								v-if="hasDueDate"
+							>
+								Due Date
+							</div> -->
+						</div>
+					</template>
+				</template>
+			</group-menu>
 			<!-- <div v-if="isMenu" class="group-edit flex f-col">
 				<button @click="emitDelete(group.id)">Delete Group</button>
 			</div> -->
@@ -74,6 +126,7 @@
 import draggable from 'vuedraggable'
 import cardPreview from '../card/card-preview.cmp';
 import groupMenu from './group-menu.cmp'
+import { utilService } from '@/services/util.service'
 
 export default {
 	props: {
@@ -81,7 +134,9 @@ export default {
 		labels: Array,
 		activities: Array,
 		filterBy: Object,
-		isCloseAdd: Boolean
+		isCloseAdd: Boolean,
+		board: Object,
+		groupIdx: Number
 	},
 	data() {
 		return {
@@ -89,6 +144,9 @@ export default {
 			newCardTxt: '',
 			isMenu: false,
 			isDrag: false,
+			isMove: false,
+			isSort: false,
+			pos: null,
 		}
 	},
 	computed: {
@@ -106,11 +164,16 @@ export default {
 					(!this.filterBy.membersIds.length || this.filterBy.membersIds.every(id => card.members.some(member => member._id === id)))
 			})
 			return cards
+		},
+		hasDueDate() {
+			return this.group.cards.some(card => card.dueDate)
 		}
 	},
 	methods: {
 		toggleMenu() {
 			this.isMenu = !this.isMenu;
+			this.isMove = false;
+			this.isSort = false;
 		},
 		addCard() {
 			this.isAdding = true;
@@ -138,6 +201,54 @@ export default {
 		update() {
 			this.emitChange();
 		},
+		openMove() {
+			this.isMove = true;
+		},
+		openSort() {
+			this.isSort = true;
+		},
+		cloneGroup() {
+			var newGroup = utilService.deepCopy(this.group);
+			newGroup.id = utilService.makeId();
+			var numOfCards = newGroup.cards.length;
+			for (let i = 0; i < numOfCards; i++) {
+				newGroup.cards[i].id = utilService.makeId();
+			}
+			this.$emit('addClone', newGroup)
+		},
+		changeGroupPos(newPos) {
+			this.$emit('moveGroup', this.groupIdx, newPos)
+			this.isMenu = false;
+			this.isMove = false;
+		},
+		backToMenu() {
+			this.isMove = false;
+			this.isSort = false;
+		},
+		sortBy(item, diff) {
+			var group = utilService.deepCopy(this.group);
+			group.cards.sort((a, b) => {
+				if (item === 'title') {
+					if (a.title.toLowerCase() < b.title.toLowerCase()) return -1 * diff;
+					if (a.title.toLowerCase() > b.title.toLowerCase()) return 1 * diff;
+					return 0;
+				} else {
+					// if (item === 'dueDate') {
+					// 	if (!a.dueDate && b.dueDate) return -1
+					// 	else if (a.dueDate && !b.dueDate) return 1
+					// 	else if (!a.dueDate && !b.dueDate) return 0
+					// }
+					if (a[item] < b[item]) return -1 * diff;
+					if (a[item] > b[item]) return 1 * diff;
+					return 0;
+				}
+			})
+			this.$emit('updateGroup', group)
+		}
+
+	},
+	created() {
+		this.pos = this.groupIdx + 1
 	},
 	components: {
 		cardPreview,
