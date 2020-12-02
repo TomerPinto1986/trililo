dc61dde020509027dd065b31355b545f9fa32a89
Shifra Sherrow <yy306090@gmail.com>
2020-12-02 22:48:27 +0200

    l

--- frontend/src/views/card-details.vue
+++ frontend/src/views/card-details.vue
@@ -109,7 +109,6 @@
                     <span class="card-icon">
                         <img src="@/assets/svg/desc.svg" />
                     </span>
-
                     <card-description
                         :description="card.description"
                         @updateDesc="updateDesc"
@@ -153,7 +152,7 @@
                     </div>
                 </div>
                 <div class="activity flex">
-                    <span class="card-icon">
+                    <span class="card-icon activity-icon">
                         <img src="@/assets/svg/activities.svg" />
                     </span>
 
