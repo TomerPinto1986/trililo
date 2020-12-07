dd888bbe7005f63600c37f8e83281c37a23a35a6
Shifra Sherrow <yy306090@gmail.com>
2020-12-07 21:26:28 +0200

    l

--- frontend/src/views/board-list.vue
+++ frontend/src/views/board-list.vue
@@ -29,7 +29,6 @@ import boardPreview from '../cmps/board/board-preview.cmp';
 const Swal = require('sweetalert2');
 
 export default {
-<<<<<<< HEAD
     data() {
         return {
             newBoardTxt: '',
@@ -93,79 +92,5 @@ export default {
     components: {
         boardPreview
     }
-=======
-	data() {
-		return {
-			isAdding: false,
-			newBoardTxt: '',
-		}
-	},
-	computed: {
-		boards() {
-			return this.$store.getters.boards
-		},
-		favoritesBoards() {
-			return this.boards.filter(board => board.isMarked)
-		},
-		otherBoards() {
-			return this.boards.filter(board => !board.isMarked)
-
-		},
-		loggedinUser() {
-			return this.$store.getters.loggedinUser;
-		}
-	},
-	methods: {
-		async deleteBoard(boardId) {
-			const userAnc = await Swal.fire({
-				position: 'center',
-				title: 'Are you sure you want to delete this board?',
-				showCancelButton: true,
-				showConfirmButton: true,
-				confirmButtonColor: '#ff505b',
-				cancelButtonColor: '#455a64',
-				confirmButtonText: 'Delete'
-			});
-			if (!userAnc.isConfirmed) return;
-			this.$store.dispatch('deleteBoard', boardId);
-		},
-		addBoard() {
-			this.isAdding = true;
-		},
-		async saveBoard() {
-			const savedBoard = await this.$store.dispatch('createNewBoard');
-			this.$router.push(`/board/${savedBoard._id}`);
-		},
-		// async saveBoard() {
-		// 	if (this.newBoardTxt) {
-		// 		const savedBoard = await this.$store.dispatch('createNewBoard', this.newBoardTxt);
-		// 		this.$router.push(`/board/${savedBoard._id}`);
-		// 	}
-		// 	this.newBoardTxt = '';
-		// 	this.isAdding = false;
-		// },
-		boardBg(bgc) {
-			const bgSrcs = this.$store.getters.sBgSrcs;
-			const style = (bgc.includes('rgb')) ? bgc : `url(${bgSrcs[+bgc]})`;
-			return style;
-		}
-	},
-	// watch: {
-	// 	'loggedInUser'() {
-	// 		this.$store.dispatch('loadBoards')
-	// 	}
-	// },
-	mounted() {
-		window.scrollTo(0, 0);
-	},
-	created() {
-		this.$store.dispatch('loadBoards')
-		this.$store.dispatch('loadUsers');
-		this.$store.commit('loadBgc');
-	},
-	components: {
-		boardPreview
-	}
->>>>>>> e538fbbc58d5713abf10248f5316ef5b9ef64f2c
 }
 </script>
